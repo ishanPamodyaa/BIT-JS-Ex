f75f02f5b8136f885688a00d2e4472216cdfafac
Ishan Pamodya <inishan9999@gmail.com>
2025-01-20 12:40:15 +0530

    tsk 01

---
+++ 250110/a.php
@@ -0,0 +1,4 @@
+<?php
+
+echo '[{"id":"1","name":"John", "age":"30"}, {"id":"2","name":"Doe", "age":"25"}]';
+?>
---
+++ ajax 3 copy/test.php
@@ -0,0 +1,17 @@
+<?php
+echo '[
+    {
+      "name": "Ishan",
+      "age": 30
+    },
+    {
+      "name": "Nuwan",
+      "age": 20
+    },
+    {
+      "name": "Kasun",
+      "age": 40
+    }
+  ]';
+  
+?>
